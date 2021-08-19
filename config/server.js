6dbcf22772d98fd6a03fb0d36b535e2a74f78c9c
Juan Antonio <juan.cahuana@globant.com>
2021-08-18 23:08:46 -0500

    Add base files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4d2b3bbdcceee209757ea61296778f4'),
+    },
+  },
+});
