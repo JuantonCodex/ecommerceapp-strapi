0fd252dee1635e93efe092ad4ccef64ad50a4148
Juan Antonio <juan.cahuana@globant.com>
2021-09-15 21:21:34 -0500

    Create product collection

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b4d2b3bbdcceee209757ea61296778f4'),
+      secret: env("ADMIN_JWT_SECRET", "b4d2b3bbdcceee209757ea61296778f4"),
     },
+  },
+  autoReload: {
+    enabled: true,
   },
 });
